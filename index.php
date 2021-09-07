abbf5a0c04346960b61ced94bce60082e4a8254d
victorllanos <victorhoraciollanos@gmail.com>
2021-09-06 22:27:34 -0300

    segundo

--- index.php
+++ index.php
@@ -18,20 +18,20 @@
 
     <!-- Icon -->
     <div class="fadeIn first">
-      <img src="https://www.b-cube.in/wp-content/uploads/2014/05/aditya-300x177.jpg" id="icon" alt="User Icon" />
-      <h1>Aditya News</h1>
+      <img src="img\logo.jpg" id="icon" alt="User Icon" />
+      <h1>Gestor de archivos</h1>
     </div>
 
     <!-- Login Form -->
     <form>
       <input type="text" id="login" class="fadeIn second" name="login" placeholder="username">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+      <input type="password" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Entrar">
     </form>
 
     <!-- Remind Passowrd -->
     <div id="formFooter">
-      <a class="underlineHover" href="#">Go to the Site</a>
+      <a class="underlineHover" href="registro.php">Registrarse</a>
     </div>
 
   </div>
